1bc03579aa47c588a30fe7411e32c22298a569bc
Onesco <Okonkwoonesmus5@gmail.com>
2023-01-13 21:52:53 +0100

    added cor

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from utils.utilities import Util
 from utils.cpu_power import get_process_power
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ["*"],
+    allow_headers = ["*"],
+    allow_methods = ["*"],
+)
+
 
 @app.get('/powers/')
-def read_powers():
-    sleep = 10
+def read_powers(sleep: int = 2, limit: int = 100, page_num= 1):
     cpu_power = get_process_power(sleep);
     util = Util(cpu_power)
-
     cpu_power = util.cpu_power
     cpu_percent = util.cpu_percent
-    running_process_power = util.get_running_process_power(limit=100, page_num=1)
+    running_process_power = util.get_running_process_power(limit, page_num)
 
     return {
        "cpu_power": cpu_power,
